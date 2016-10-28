6c04ac0e0bf02ee9c8d36053a5db95e070d2efd6
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-10-28 17:19:39 +0700

    Add Post Entity

--- src/main/java/web/Member.java
+++ src/main/java/web/Member.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 @Entity @Table(name="member")
 public class Member {
 	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
-	Long id;
+	long id;
 	String email;
 	String password;
 	String name;
